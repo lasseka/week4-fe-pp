a1ac76d3e483ca2aa62948f7e274408acd9aa519
Lasse Kauranen <lasse.kauranen@metropolia.fi>
2024-11-13 09:50:12 +0200

    Iteration 2 complete

---
+++ src/components/Services.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { services } from '../data'
+import Title from './Title'
+import Service from './Service'
+
+const Services = () => {
+
+  const [servicesData, setServicesData] = useState(services)
+  return (
+    <section className='section services' id='services'>
+      <Title title='our' subTitle='services' />
+
+      <div className='section-center services-center'>
+        {servicesData.map((service) => {
+          return <Service {...service} key={service.id} />
+        })}
+      </div>
+    </section>
+  )
+}
+export default Services
